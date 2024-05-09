234930c6df1d2811a7a1345d700684b18733f38e
DeongareeSniper <782381967@qq.com>
2024-05-09 17:30:31 +0800

    新增光谱函数索引，修改小数转换，以及新增查询目标波段函数

---
+++ include/SpectralIndices.h
@@ -0,0 +1,18 @@
+enum spectralIndices
+{
+    bai,
+    nbr,
+    nbrt1,
+    cmr,
+    fmr,
+    ior,
+    wvii,
+    wvsi,
+    mndwi,
+    ndbi,
+    ndmi,
+    ndsi,
+    wvbi,
+    wvnhfd,
+    wvwi
+};
--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@ Email：782381967@qq.com
 
 #include <iostream>
 #include <cmath>
+#include "../include/SpectralIndices.h"
 using namespace std;
 // Burn Indices Background
 float burn_area_index(int red, int nir)
@@ -21,7 +22,7 @@ float burn_area_index(int red, int nir)
     return bai;
 }
 
-float normalized_burn_ratio(float swir, float nir)
+float normalized_burn_ratio(int swir, int nir)
 {
     float a, b;
     float nbr;
@@ -31,7 +32,7 @@ float normalized_burn_ratio(float swir, float nir)
     return nbr;
 }
 
-float normalized_burn_ratio_thermal_1(float swir, float nir, float thermal)
+float normalized_burn_ratio_thermal_1(int swir, int nir, int thermal)
 {
     float a, b;
     float nbrt1;
@@ -42,36 +43,36 @@ float normalized_burn_ratio_thermal_1(float swir, float nir, float thermal)
 }
 
 // Geology Indices Background
-float clay_minerals_ratio(float swir1, float swir2)
+float clay_minerals_ratio(int swir1, int swir2)
 {
     float cmr;
     cmr = swir1 / swir2;
     return cmr;
 }
 
-float ferrous_minerals_ratio(float swir, float nir)
+float ferrous_minerals_ratio(int swir, int nir)
 {
     float fmr;
     fmr = swir / nir;
     return fmr;
 }
 
-float iron_oxide_ratio(float red, float blue)
+float iron_oxide_ratio(int red, int blue)
 {
     float ior;
     ior = red / blue;
     return ior;
 }
 
-float worldview_new_iron_index(float green, float yellow, float blue)
+float worldview_new_iron_index(int green, int yellow, int blue)
 {
     float wvii;
-    wvii = (green - yellow) / (blue * 1000);
+    wvii = ((float)green - (float)yellow) / ((float)blue * 1000);
     cout << "WV-II is " << wvii << endl;
     return wvii;
 }
 
-float worldview_soil_index(float green, float yellow)
+float worldview_soil_index(int green, int yellow)
 {
     float wvsi;
     wvsi = (green - yellow) / (green + yellow);
@@ -79,66 +80,97 @@ float worldview_soil_index(float green, float yellow)
 }
 
 // Miscellaneous Indices Background
-float modified_normalized_difference_water_index(float green, float swir)
+float modified_normalized_difference_water_index(int green, int swir)
 {
     float mndwi;
     mndwi = (green - swir) / (green + swir);
     return mndwi;
 }
 
-float normalized_difference_built_up_index(float swir, float nir)
+float normalized_difference_built_up_index(int swir, int nir)
 {
     float ndbi;
     ndbi = (swir - nir) / (swir + nir);
     return ndbi;
 }
 
-float normalized_difference_mud_index(float p795, float p990)
+float normalized_difference_mud_index(int p795, int p990)
 {
     float ndmi;
     ndmi = (p795 - p990) / (p795 + p990);
     return ndmi;
 }
 
-float normalized_difference_snow_index(float green, float nir)
+float normalized_difference_snow_index(int green, int nir)
 {
     float ndsi;
     ndsi = (green - nir) / (green + nir);
     return ndsi;
 }
 
-float worldview_built_up_index(float coastal, float red_edge)
+float worldview_built_up_index(int coastal, int red_edge)
 {
     float wvbi;
     wvbi = (coastal - red_edge) / (coastal + red_edge);
     return wvbi;
 }
 
-float worldView_non_homogeneous_feature_difference(float red_edge, float coastal)
+float worldView_non_homogeneous_feature_difference(int red_edge, int coastal)
 {
     float wvnhfd;
     wvnhfd = (red_edge - coastal) / (red_edge + coastal);
     return wvnhfd;
 }
 
-float worldview_water_index(float coastal, float nir2)
+float worldview_water_index(int coastal, int nir2)
 {
     float wvwi;
     wvwi = (coastal - nir2) / (coastal + nir2);
     return wvwi;
 }
 
-inline float transform_to_six_decimal_places(float x)
+// float转化为6位小数，并四舍五入
+float transform_to_six_decimal_places(float x)
 {
-    float a,c;
-    int b;
-    a = x*1000000;
-    b=(int)a;
-    c = (float)b/1000000;
-    return c;
+    float a, b;
+    a = x * 1000000;
+    b = round(a) / 1000000;
+    return b;
 }
 
-//testing
+// 判断所寻找的中心波段，确定该波段范围，并判断该波段是否符合范围要求
+int determine_band_range(float value_of_band, int value_of_needed_band) {
+    // switch(value_of_needed_band)
+    // {
+    //     case
+    // }
+};
+
+// 寻找中心波段,返回查找的波段的编号,后续还需要确定是否在波段范围内
+int find_central_band(float *value_array_of_all_band, int size_of_all_band, int value_of_needed_band)
+{
+    int i;
+    float j, k, x, y;
+    int band_number = -1;
+    int sub_value = 1000;
+    for (i = 0; i < size_of_all_band; i++)
+    {
+        x = value_array_of_all_band[i];
+        y = determine_band_range(x, value_of_needed_band);
+        if (y == 1)
+        {
+            j = x - (float)value_of_needed_band;
+            k = fabs(j);
+            if (k < sub_value)
+            {
+                band_number = i;
+            }
+        }
+    }
+    return band_number;
+}
+
+// testing
 int main(int argc, const char **argv)
 {
     int green, yellow, blue;
@@ -146,6 +178,6 @@ int main(int argc, const char **argv)
     yellow = 499;
     blue = 124;
     float wvii = transform_to_six_decimal_places(worldview_new_iron_index(green, yellow, blue));
-    std::cout << "WV_II is " << wvii << std::endl;
+    cout << "WV_II is " << wvii << endl;
     return 0;
 }
