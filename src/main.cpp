783e30e86292898da0e893a3196a6ab3371bfba0
DeongareeSniper <782381967@qq.com>
2024-05-15 19:11:40 +0800

    修复了无法正确返回中心波段的序号的bug，新增判断波段范围时可以取到边界值

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@ Email：782381967@qq.com
 主题：ENVI光谱函数库
 时间：2024-01-15
 
-备注：传入参数为像素DM值
+备注：传入参数为像素DN值
 */
 
 #include "../include/SpectralIndices.h"
@@ -17,9 +17,14 @@ using namespace std;
 #define NIR 861
 #define SWIR1 1651
 #define SWIR2 2221
-#define VREI2 716
-#define RENDVI 751
-#define ARVI 801
+#define p715_wide 716
+#define p750_narrow 751
+#define p800_wide 801
+
+// 说明文档里没有，无法确定中心波长，用0占位，以下参数用于wvbi,wvnhfd,wvwi
+#define Red_Edge 0
+#define Coastal 0
+#define NIR2 0
 
 // float转化为6位小数，并四舍五入
 float transform_to_six_decimal_places(float x)
@@ -36,187 +41,187 @@ int determine_band_range(float value_of_band, int value_of_needed_band)
     switch (value_of_needed_band)
     {
     case 445:
-        if (value_of_band > 435 && value_of_band < 448)
+        if (value_of_band >= 435 && value_of_band <= 448)
             return 1;
         break;
     case 450:
-        if (value_of_band > 425 && value_of_band < 475)
+        if (value_of_band >= 425 && value_of_band <= 475)
             return 1;
         break;
     case BLUE: // 470
-        if (value_of_band > 400 && value_of_band < 500)
+        if (value_of_band >= 400 && value_of_band <= 500)
             return 1;
         break;
     case 500:
-        if (value_of_band > 480 && value_of_band < 520)
+        if (value_of_band >= 480 && value_of_band <= 520)
             return 1;
         break;
     case 510:
-        if (value_of_band > 500 && value_of_band < 515)
+        if (value_of_band >= 500 && value_of_band <= 515)
             return 1;
         break;
     case 531:
-        if (value_of_band > 525 && value_of_band < 550)
+        if (value_of_band >= 525 && value_of_band <= 550)
             return 1;
         break;
     case 550:
-        if (value_of_band > 540 && value_of_band < 560)
+        if (value_of_band >= 540 && value_of_band <= 560)
             return 1;
         break;
     case GREEN: // 550
-        if (value_of_band > 500 && value_of_band < 600)
+        if (value_of_band >= 500 && value_of_band <= 600)
             return 1;
         break;
     case 570:
-        if (value_of_band > 560 && value_of_band < 575)
+        if (value_of_band >= 560 && value_of_band <= 575)
             return 1;
         break;
     case RED: // 650
-        if (value_of_band > 600 && value_of_band < 700)
+        if (value_of_band >= 600 && value_of_band <= 700)
             return 1;
         break;
     case 670:
-        if (value_of_band > 650 && value_of_band < 690)
+        if (value_of_band >= 650 && value_of_band <= 690)
             return 1;
         break;
     case 699:
-        if (value_of_band > 650 && value_of_band < 735)
+        if (value_of_band >= 650 && value_of_band <= 735)
             return 1;
         break;
     case 700:
-        if (value_of_band > 680 && value_of_band < 730)
+        if (value_of_band >= 680 && value_of_band <= 730)
             return 1;
         break;
     case 705:
-        if (value_of_band > 697 && value_of_band < 708)
+        if (value_of_band >= 697 && value_of_band <= 708)
             return 1;
         break;
     case 715:
-        if (value_of_band > 714 && value_of_band < 716)
+        if (value_of_band >= 714 && value_of_band <= 716)
             return 1;
         break;
-    case VREI2: // 715
-        if (value_of_band > 710 && value_of_band < 719)
+    case p715_wide: // 715
+        if (value_of_band >= 710 && value_of_band <= 719)
             return 1;
         break;
     case 720:
-        if (value_of_band > 718 && value_of_band < 722)
+        if (value_of_band >= 718 && value_of_band <= 722)
             return 1;
         break;
     case 726:
-        if (value_of_band > 725 && value_of_band < 727)
+        if (value_of_band >= 725 && value_of_band <= 727)
             return 1;
         break;
     case 734:
-        if (value_of_band > 730 && value_of_band < 736)
+        if (value_of_band >= 730 && value_of_band <= 736)
             return 1;
         break;
     case 740:
-        if (value_of_band > 730 && value_of_band < 750)
+        if (value_of_band >= 730 && value_of_band <= 750)
             return 1;
         break;
     case 747:
-        if (value_of_band > 742 && value_of_band < 748)
+        if (value_of_band >= 742 && value_of_band <= 748)
             return 1;
         break;
     case 750:
-        if (value_of_band > 730 && value_of_band < 780)
+        if (value_of_band >= 730 && value_of_band <= 780)
             return 1;
         break;
-    case RENDVI: // 750
-        if (value_of_band > 730 && value_of_band < 760)
+    case p750_narrow: // 750
+        if (value_of_band >= 730 && value_of_band <= 760)
             return 1;
         break;
     case 795:
-        if (value_of_band > 720 && value_of_band < 800)
+        if (value_of_band >= 720 && value_of_band <= 800)
             return 1;
         break;
     case 800:
-        if (value_of_band > 780 && value_of_band < 865)
+        if (value_of_band >= 780 && value_of_band <= 865)
             return 1;
         break;
-    case ARVI: // 800
-        if (value_of_band > 750 && value_of_band < 870)
+    case p800_wide: // 800
+        if (value_of_band >= 750 && value_of_band <= 870)
             return 1;
         break;
     case 819:
-        if (value_of_band > 815 && value_of_band < 824)
+        if (value_of_band >= 815 && value_of_band <= 824)
             return 1;
         break;
     case 857:
-        if (value_of_band > 854 && value_of_band < 860)
+        if (value_of_band >= 854 && value_of_band <= 860)
             return 1;
         break;
     case 860:
-        if (value_of_band > 841 && value_of_band < 876)
+        if (value_of_band >= 841 && value_of_band <= 876)
             return 1;
         break;
     case NIR: // 860
-        if (value_of_band > 760 && value_of_band < 960)
+        if (value_of_band >= 760 && value_of_band <= 960)
             return 1;
         break;
     case 900:
-        if (value_of_band > 860 && value_of_band < 910)
+        if (value_of_band >= 860 && value_of_band <= 910)
             return 1;
         break;
     case 970:
-        if (value_of_band > 965 && value_of_band < 975)
+        if (value_of_band >= 965 && value_of_band <= 975)
             return 1;
         break;
     case 990:
-        if (value_of_band > 830 && value_of_band < 995)
+        if (value_of_band >= 830 && value_of_band <= 995)
             return 1;
         break;
     case 1241:
-        if (value_of_band > 1230 && value_of_band < 1250)
+        if (value_of_band >= 1230 && value_of_band <= 1250)
             return 1;
         break;
     case 1510:
-        if (value_of_band > 1500 && value_of_band < 1515)
+        if (value_of_band >= 1500 && value_of_band <= 1515)
             return 1;
         break;
     case 1599:
-        if (value_of_band > 1590 && value_of_band < 1620)
+        if (value_of_band >= 1590 && value_of_band <= 1620)
             return 1;
         break;
     case 1640:
-        if (value_of_band > 1628 && value_of_band < 1652)
+        if (value_of_band >= 1628 && value_of_band <= 1652)
             return 1;
         break;
     case 1649:
-        if (value_of_band > 1645 && value_of_band < 1655)
+        if (value_of_band >= 1645 && value_of_band <= 1655)
             return 1;
         break;
     case SWIR1: // 1650
-        if (value_of_band > 1550 && value_of_band < 1750)
+        if (value_of_band >= 1550 && value_of_band <= 1750)
             return 1;
         break;
     case 1680:
-        if (value_of_band > 1670 && value_of_band < 1690)
+        if (value_of_band >= 1670 && value_of_band <= 1690)
             return 1;
         break;
     case 1754:
-        if (value_of_band > 1750 && value_of_band < 1758)
+        if (value_of_band >= 1750 && value_of_band <= 1758)
             return 1;
         break;
     case 2000:
-        if (value_of_band > 1980 && value_of_band < 2040)
+        if (value_of_band >= 1980 && value_of_band <= 2040)
             return 1;
         break;
     case 2100:
-        if (value_of_band > 2085 && value_of_band < 2110)
+        if (value_of_band >= 2085 && value_of_band <= 2110)
             return 1;
         break;
     case 2130:
-        if (value_of_band > 2105 && value_of_band < 2155)
+        if (value_of_band >= 2105 && value_of_band <= 2155)
             return 1;
         break;
     case 2200:
-        if (value_of_band > 2170 && value_of_band < 2220)
+        if (value_of_band >= 2170 && value_of_band <= 2220)
             return 1;
         break;
     case SWIR2: // 2220
-        if (value_of_band > 2080 && value_of_band < 2350)
+        if (value_of_band >= 2080 && value_of_band <= 2350)
             return 1;
         break;
     }
@@ -229,11 +234,11 @@ int find_central_band(float *value_array_of_all_band, int size_of_all_band, int 
     int i, a;
     float j, k, x, y;
     int band_number = -1;
-    int sub_value = 1000;
+    float sub_value = 1000;
     a = value_of_needed_band;
     if (a == BLUE || a == RED || a == GREEN ||
         a == NIR || a == SWIR1 || a == SWIR2 ||
-        a == VREI2 || a == RENDVI || a == ARVI)
+        a == p715_wide || a == p750_narrow || a == p800_wide)
     {
         a--;
     }
@@ -248,6 +253,7 @@ int find_central_band(float *value_array_of_all_band, int size_of_all_band, int 
             if (k < sub_value)
             {
                 band_number = i;
+                sub_value = k;
             }
         }
     }
@@ -255,19 +261,18 @@ int find_central_band(float *value_array_of_all_band, int size_of_all_band, int 
 }
 
 // 光谱指数计算模块对外接口
-void spectral_index_calculation(int ***raw_data, int x, int y, int z, float *value_array_of_all_band,
-                                SpectralIndices spectralindices_index, float **result)
+int spectral_index_calculation(int ***raw_data, int x, int y, int z, float *value_array_of_all_band,
+                               SpectralIndices spectralindices_index, float **result)
 {
+    
 }
 
 // testing
 int main(int argc, const char **argv)
 {
-    int green, yellow, blue;
-    green = 657;
-    yellow = 499;
-    blue = 124;
-    float wvii = transform_to_six_decimal_places(worldview_new_iron_index(green, yellow, blue));
-    cout << "WV_II is " << wvii << endl;
+    float a[10] = {100.65465, 200, 380.5648, 420.98216, 469.9999, 470.1, 498, 500, 550.854, 600};
+    int b;
+    b = find_central_band(a, 10, BLUE);
+    cout << "band is " << b << endl;
     return 0;
 }
